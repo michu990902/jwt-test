b2901efc5fdbf850f0867a0c61c74dcda7ad28e1
michu990902 <41807744+michu990902@users.noreply.github.com>
2020-07-15 03:54:00 +0200

    Middleware test

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module jwt-test
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/jinzhu/gorm v1.9.14 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/wader/gormstore v0.0.0-20200328121358-65a111a20c23 // indirect
 )
--- main.go
+++ main.go
@@ -14,11 +14,17 @@ import (
 	"github.com/gin-gonic/gin"	
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	
+	// jwt "github.com/dgrijalva/jwt-go"
+	// "github.com/gorilla/sessions"
+	"github.com/wader/gormstore"
 )
 
 type Server struct {
 	DB     *gorm.DB
 	Router *gin.Engine
+	Store  *gormstore.Store
 }
 
 type User struct{
@@ -55,6 +61,18 @@ func (u *User) Prepare() {
 	u.UpdatedAt = time.Now()
 }
 
+func (s *Server) CreateUser(nickname, email, password string){
+	u := User{
+		Nickname: nickname,
+		Email: email,
+		Password: password,
+	}
+	u.BeforeSave()
+	u.Prepare()
+	err := s.DB.Create(&u).Error
+	fmt.Println(err)
+}
+
 var server = Server{}
 
 func main(){
@@ -73,32 +91,156 @@ func main(){
 	server.DB.AutoMigrate(&User{})
 	server.Router = gin.Default()
 
+
+	//
+	server.Store = gormstore.New(server.DB, []byte(os.Getenv("API_SECRET")))
+	// db cleanup every hour
+	// close quit channel to stop cleanup
+	quit := make(chan struct{})
+	go server.Store.PeriodicCleanup(1*time.Hour, quit)
+	//
+
+	//
+	server.CreateUser("test1", "test@test.pl", "1234")
+	server.CreateUser("test2", "test2@test.pl", "zaq1@WSX")
+	//
+
 	server.Router.LoadHTMLGlob("templates/*")
 
 	server.Router.GET("/", server.Home)
-	server.Router.GET("/test", server.Test)
+	// server.Router.GET("/test", server.Test)
+	// server.Router.GET("/test", server.isLoggedIn(server.Test))
+	
 	server.Router.POST("/singin", server.SingIn)
 	server.Router.GET("/singout", server.SingOut)
+
+	authorized := server.Router.Group("/")
+
+	authorized.Use(isLoggedIn())
+	{
+		authorized.GET("/test", server.Test)
+	}
 
 	server.Router.Run(":8080")
 }
 
 func (s *Server) Home(c *gin.Context){
+	//
+	// s.AddUserToStore(c, 1)
+	//
 	c.HTML(http.StatusOK, "index.gohtml", gin.H{
 		"title": "Main website",
 	})
 }
 
 func (s *Server) Test(c *gin.Context){
+	//
+	s.GetUserFromStore(c)
+	//
 	c.HTML(http.StatusOK, "test.gohtml", gin.H{
 		"title": "Test",
 	})
 }
 
 func (s *Server) SingIn(c *gin.Context){
+	var tmpUser, user User
+	var err error
 
+	err = c.ShouldBindJSON(&tmpUser)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	//auth
+	err = s.DB.Where("email = ?", tmpUser.Email).First(&user).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	err = VerifyPassword(user.Password, tmpUser.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	// token, err := CreateToken(user.ID)
+	// if err != nil {
+	//    	c.JSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"message": err.Error(),
+	// 	})
+	//    return
+	// }
+	// c.Redirect(http.StatusOK, "/")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login succesfull",
+	})
 }
 
 func (s *Server) SingOut(c *gin.Context){
 	
+	c.Redirect(http.StatusOK, "/")
+}
+
+func (s *Server) AddUserToStore(c *gin.Context, userID uint64){
+	session, err := s.Store.Get(c.Request, "mind-map-session")
+	if err != nil {
+		fmt.Println("store error:", err.Error())
+		//json internal server error
+		return
+	}
+	session.Values["user_id"] = userID
+	session.Values["user_role"] = "admin" 
+	//termin wygasania sesji
+	session.Values["exp"] = time.Now().Add(time.Hour * 1).Unix()
+	s.Store.Save(c.Request, c.Writer, session)
+}
+
+func (s *Server) GetUserFromStore(c *gin.Context){
+	session, err := s.Store.Get(c.Request, "mind-map-session")
+	if err != nil {
+		fmt.Println("store error:", err.Error())
+		//json internal server error
+		return
+	}
+	userID := session.Values["user_id"].(uint64)
+	userRole := session.Values["user_role"].(string)
+	fmt.Println("logged user:", userID, "(", userRole, ")")
+}
+
+
+func isLoggedIn() gin.HandlerFunc {	
+	return func (c *gin.Context){
+		_, err := c.Cookie("mind-map-session")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
+			})
+			return
+		}
+		c.Next()
+	}
+
+	// session, err := s.Store.Get(c.Request, "session")
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{
+	// 		"message": err.Error(),
+	// 	})
+	// 	return
+	// }
+
+	// userID := session.Values["user_id"].(uint64)
+	// userRole := session.Values["role"].(string)
+	
+	// license := session.Values["license_type"].(bool)
+	//!!!licencja z bazy
+
+
 }
